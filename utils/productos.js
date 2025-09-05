bf5018f0a2214ea909c6f2796b2a40adee5857ff
Jeronimo Cardu <jeronimocardu29@gmail.com>
2025-09-05 10:32:14 -0300

    feat: funcionalidad de buscador y refactorizacion

--- utils/productos.js
+++ utils/productos.js
@@ -1,28 +1,54 @@
 const inputSearch = document.getElementById("search");
 const productosContainer = document.getElementById("productos-container");
+const formSearch = document.getElementById("form-search");
 
-async function getProductos() {
+// Funcion que añade cada producto al main (productosContainer)
+function showProduct(product) {
+  productosContainer.innerHTML += `
+        <article class="articuloDeProducto">
+        <a class="linkAProducto" href="producto.html?id=${product.id}">
+        <img src="${product.img}" alt="${product.nombre}"/> 
+        <div class="infoDeArticulo">
+        <h1>${product.nombre}</h1>
+        <p>${product.descripcion}</p>
+        </div>
+        </a>
+        </article>
+        `;
+}
+
+// iteracion de productos ( todos o por filtrado)
+async function getProductos(filter = "") {
+  productosContainer.innerHTML = "<h1 class='cargando'>Cargando...</h1>";
+  await delay(1500);
+
   productosContainer.innerHTML = "";
   productos.forEach((product) => {
-    productosContainer.innerHTML += `
-    <article class="articuloDeProducto">
-        <a class="linkAProducto" href="producto.html?id=${product.id}">
-            <img src="${product.img}" alt="${product.nombre}"/> 
-            <div class="infoDeArticulo">
-                <h1>${product.nombre}</h1>
-                <p>${product.descripcion}</p>
-            </div>
-        </a>
-    </article>
-    `;
+    if (filter !== "") {
+      if (
+        product.nombre.toLowerCase().includes(filter.toLowerCase()) ||
+        product.descripcion.toLowerCase().includes(filter.toLowerCase())
+      ) {
+        showProduct(product);
+      }
+    } else {
+      showProduct(product);
+    }
   });
 }
 
-let loading = true;
-if (loading) {
-  productosContainer.innerHTML = "<h1 class='cargando'>Cargando...</h1>";
+// Simulacion de asincronía
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
-setTimeout(() => {
-  getProductos();
-  loading = false;
-}, 1500);
+
+// Events
+inputSearch.addEventListener("input", (e) => {
+  getProductos(e.target.value);
+});
+
+formSearch.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+getProductos();
