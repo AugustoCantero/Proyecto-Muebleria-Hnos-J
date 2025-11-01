3d0c7d5c6d2c1e8767a3d4bdddee7476e3506fe6
Jeronimo Cardu <jeronimocardu29@gmail.com>
2025-11-01 16:45:07 -0300

    peticion get para contacts

--- backend/routes/conctacRoutes.js
+++ backend/routes/conctacRoutes.js
@@ -3,6 +3,16 @@ const router = express.Router();
 const contactModel = require("../models/ContactModel");
 
 // Ruta para manejar el formulario de contacto
+router.get("/", async (req, res) => {
+  try {
+    const contactos = await contactModel.find();
+    res.status(200).json(contactos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error al obtener los mensajes" });
+  }
+});
+
 router.post("/", async (req, res) => {
   const { name, email, message } = req.body;
   try {
