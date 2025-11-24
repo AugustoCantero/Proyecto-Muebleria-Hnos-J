4cdcb78832bac69458657017368984aee3bfb589
Amir Ale <Amir___1@outlook.com>
2025-11-24 18:02:39 -0300

    feat: authMiddleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {getUserById, createUser, loginUser}  = require("../controllers/userController");
+const authMiddleware = require('../middlewares/authMiddleware');
+const {getUserProfile, getUserById, createUser, loginUser}  = require("../controllers/userController");
 
+router.get('/profile', authMiddleware, getUserProfile);
 router.get("/:id", getUserById);
 router.post("/register", createUser);
 router.post("/login", loginUser);
