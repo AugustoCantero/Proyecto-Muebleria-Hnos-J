b5a745e2874aa1326d2c3d2edc9c7918dfe56550
Jeronimo Cardu <85565183+JeronimoCardu@users.noreply.github.com>
2025-11-24 15:53:05 -0300

    delete Context directory

--- client/src/Contexts/CartProvider.jsx
+++
@@ -1,6 +0,0 @@
-import { CartContext } from "./CartContext";
-
-export const CartProvider = ({ children }) => {
-  const value = {};
-  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
-};
